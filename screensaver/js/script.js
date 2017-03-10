aa1838ca1fbaca4f1ffadfb41e51f9cb5a0c6c6e
Bushra <sarkb908@newschool.edu>
2017-03-10 16:31:29 -0500

    Gradient
    Needs transition

--- screensaver/js/script.js
+++ screensaver/js/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-			console.log("I'm working");
-			$(".div1").addClass("div1b");
-		});
+// $(document).ready(function() {
+// 			console.log("I'm working");
+// 			$(".div1").addClass("div1b");
+// 		});
 
